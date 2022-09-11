7b5d4a2f26fab4f37dae853c87792d5cd8b33282
TWentCEO <twent1003@gmail.com>
2022-09-11 18:11:12 +0900

    5. 특정문자뒤집기

--- Inflearn/src/Qa5.java
+++ Inflearn/src/Qa5.java
@@ -1,32 +1,32 @@
-import java.util.*;
-
-class Qa5 {
-    public String solusion(String str){
+import java.util.Scanner;
+class Main{
+    public String solution(String str){
         String answer = "";
-        char[] c = str.toCharArray();
-        int lt = 0, rt = str.length()-1;
 
-        while(lt<rt){
-            if(!Character.isAlphabetic(c[lt])) lt++;
-            else if(!Character.isAlphabetic(c[rt])) rt--;
+        int lt = 0;
+        int rt = str.length()-1;
+        char[] s = str.toCharArray();
+
+        while (lt < rt){
+            if(!Character.isAlphabetic(s[lt])) lt++;
+            else if(!Character.isAlphabetic(s[rt])) rt--;
             else{
-                char tmp = c[lt];
-                c[lt] = c[rt];
-                c[rt] = tmp;
-                lt++;
-                rt--;
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt ++;
+                rt --;
             }
         }
-        answer = String.valueOf(c);
-        return answer;   
+      	answer = String.valueOf(s);
+        return answer;
     }
 
     public static void main(String[] args){
-        Qa5 T = new Qa5();
+        Main T = new Main();
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
 
-        System.out.println(T.solusion(str));
+        System.out.println(T.solution(str));
     }
-    
 }
